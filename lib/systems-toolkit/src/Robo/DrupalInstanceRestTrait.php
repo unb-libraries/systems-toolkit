dddbfe745223d2c2a636dbd8c42f65e74b8840da
Jacob Sanford <jsanford@unb.ca>
2018-10-03 21:50:08 -0300

    Add newspaper page command

--- lib/systems-toolkit/src/Robo/DrupalInstanceRestTrait.php
+++ lib/systems-toolkit/src/Robo/DrupalInstanceRestTrait.php
@@ -138,4 +138,71 @@ trait DrupalInstanceRestTrait {
     return json_decode((string) $this->drupalRestResponse->getBody());
   }
 
+  /**
+   * Create an entity via the Drupal REST client.
+   *
+   * @param string $create_uri
+   *   The patch URI.
+   * @param string $create_content
+   *   The patch content.
+   *
+   * @throws \Exception
+   *
+   * @return object
+   *   The JSON object returned from the server.
+   */
+  protected function createDrupalRestEntity($create_uri, $create_content) {
+    $this->setUpDrupalRestClientToken();
+    $create_uri = $this->drupalRestUri . "$create_uri?_format=json";
+    $this->say($create_uri);
+    $this->drupalRestResponse = $this->guzzleClient
+      ->post($create_uri, [
+        'auth' => [$this->drupalRestUser, $this->drupalRestPassword],
+        'body' => $create_content,
+        'headers' => [
+          'Content-Type' => 'application/json',
+          'X-CSRF-Token' => $this->drupalRestToken
+        ],
+      ]);
+    return json_decode((string) $this->drupalRestResponse->getBody());
+  }
+
+  /**
+   * Upload a file for an entity field using the Drupal REST client.
+   *
+   * @param string $entity_type_id
+   *   The entity type ID.
+   * @param string $entity_bundle
+   *   The entity bundle.
+   * @param string $field_name
+   *   The field name where the file will be attached.
+   * @param string $file_contents
+   *   The contents of the file to attach.
+   * @param string $file_name
+   *   The name to use for the file when attaching.
+   *
+   * @throws \Exception
+   *
+   * @return object
+   *   The JSON object of the file returned from the server.
+   */
+  protected function uploadDrupalRestFileToEntityField($entity_type_id, $entity_bundle, $field_name, $file_contents, $file_name) {
+    $this->setUpDrupalRestClientToken();
+    $post_uri = $this->drupalRestUri . "/file/upload/{$entity_type_id}/{$entity_bundle}/{$field_name}?_format=json";
+    $this->say($post_uri);
+    $this->drupalRestResponse = $this->guzzleClient->post(
+      $post_uri,
+      [
+        'auth' => [$this->drupalRestUser, $this->drupalRestPassword],
+        'body' => $file_contents,
+        'headers' => [
+          'Content-Type' => 'application/octet-stream',
+          'Content-Disposition' => "file; filename=\"$file_name\"",
+          'X-CSRF-Token' => $this->drupalRestToken
+        ],
+      ]
+    );
+    return json_decode((string) $this->drupalRestResponse->getBody());
+  }
+
 }
--- lib/systems-toolkit/src/Robo/NewspapersLibUnbCaPageOcrCommand.php
+++ lib/systems-toolkit/src/Robo/NewspapersLibUnbCaPageOcrCommand.php
@@ -13,6 +13,7 @@ class NewspapersLibUnbCaPageOcrCommand extends OcrCommand {
   use DrupalInstanceRestTrait;
 
   const NEWSPAPERS_PAGE_REST_PATH = '/digital_serial/digital_serial_page/%s';
+  const NEWSPAPERS_PAGE_CREATE_PATH = '/entity/digital_serial_page';
 
   /**
    * Download the page image file attached to an digital page entity.
@@ -132,6 +133,127 @@ class NewspapersLibUnbCaPageOcrCommand extends OcrCommand {
       ]
     );
     $this->patchDrupalRestEntity(sprintf(self::NEWSPAPERS_PAGE_REST_PATH, $id), $patch_content);
+  }
+
+  /**
+   * Create a digital serial page from a source file.
+   *
+   * @param string $issue_id
+   *   The parent issue ID.
+   * @param string $page_no
+   *   The page number.
+   * @param string $page_sort
+   *   The page sort.
+   * @param string $file_path
+   *   The path to the file.
+   *
+   * @option string $instance-uri
+   *   The URI of the target instance.
+   *
+   * @throws \Exception
+   *
+   * @usage "1 10 10 /home/jsanford/test.jpg"
+   *
+   * @command newspapers.lib.unb.ca:create-page
+   */
+  public function createSerialPageFromFile($issue_id, $page_no, $page_sort, $file_path, $options = ['instance-uri' => 'http://localhost:3095']) {
+    $this->drupalRestUri = $options['instance-uri'];
+
+    // Do OCR on file.
+    $this->ocrTesseractFile(
+      $file_path,
+      $options = [
+        'oem' => 1,
+        'lang' => 'eng',
+        'args' => 'hocr',
+      ]
+    );
+
+    // Distill down HOCR.
+    $hocr_content = file_get_contents($file_path . ".hocr");
+    $ocr_content = strip_tags($hocr_content);
+
+    // Upload file to field.
+    $file_contents = file_get_contents($file_path);
+    $file_entity = $this->uploadDrupalRestFileToEntityField(
+      'digital_serial_page', 'digital_serial_page', 'page_image', $file_contents, 'test.jpg'
+    );
+
+    // Create digital page
+    $create_content = json_encode(
+      [
+        'parent_issue' => [
+          [
+            'target_id' => $issue_id,
+          ]
+        ],
+        'page_no' => [
+          [
+            'value' => $page_no,
+          ]
+        ],
+        'page_sort' => [
+          [
+            'value' => $page_sort,
+          ]
+        ],
+        'page_ocr' => [
+          [
+            'value' => $ocr_content,
+          ]
+        ],
+        'page_hocr' => [
+          [
+            'value' => $hocr_content,
+          ]
+        ],
+        'page_image' => [
+          [
+            'target_id' => $file_entity->fid[0]->value,
+          ]
+        ],
+      ]
+    );
+
+    $this->createDrupalRestEntity(self::NEWSPAPERS_PAGE_CREATE_PATH, $create_content);
+  }
+
+  /**
+   * Upload a file for an entity field using the Drupal REST client.
+   *
+   * @param string $entity_type_id
+   *   The entity type ID.
+   * @param string $entity_bundle
+   *   The entity bundle.
+   * @param string $field_name
+   *   The field name where the file will be attached.
+   * @param string $file_contents
+   *   The contents of the file to attach.
+   * @param string $file_name
+   *   The name to use for the file when attaching.
+   *
+   * @throws \Exception
+   *
+   * @return object
+   *   The JSON object of the file returned from the server.
+   */
+  protected function uploadDrupalRestFileToEntityField($entity_type_id, $entity_bundle, $field_name, $file_contents, $file_name) {
+    $this->setUpDrupalRestClientToken();
+    $post_uri = $this->drupalRestUri . "/file/upload/{$entity_type_id}/{$entity_bundle}/{$field_name}?_format=json";
+    $this->say($post_uri);
+    $this->drupalRestResponse = $this->guzzleClient->post(
+      $post_uri,
+      [
+        'auth' => [$this->drupalRestUser, $this->drupalRestPassword],
+        'body' => $file_contents,
+        'headers' => [
+          'Content-Type' => 'application/octet-stream',
+          'Content-Disposition' => "file; filename=\"$file_name\"",
+          'X-CSRF-Token' => $this->drupalRestToken
+        ],
+      ]
+    );
+    return json_decode((string) $this->drupalRestResponse->getBody());
   }
 
 }
