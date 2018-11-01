440c045e1a53050ccef1f4f4734f8dfcbbda5dbd
jsanford <jsanford@unb.ca>
2018-11-01 15:01:22 -0300

    NBNP-198 Update import command to reflect order of m-d-Y in metadata

--- lib/systems-toolkit/rewriteConfigFile.php
+++ lib/systems-toolkit/rewriteConfigFile.php
@@ -8,6 +8,10 @@ if (file_exists($argv[1])) {
 
   $metadata_contents = file_get_contents($argv[1]);
   $metadata_contents = str_replace('<?php', '', $metadata_contents);
+
+  // Reverse month and day in file. This is due to an error in standards.
+  fixDate($metadata_contents);
+
   eval($metadata_contents);
 
   $config = [
@@ -23,4 +27,18 @@ if (file_exists($argv[1])) {
   ];
 
   file_put_contents($argv[1] . ".json", json_encode($config));
+}
+
+function fixDate(&$metadata_contents) {
+  $matches=[];
+  preg_match('/mktime\((.*?)\)/', $metadata_contents, $matches);
+  if (!empty($matches[1])) {
+    $date_array = explode(',', $matches[1]);
+    $temp_val = $date_array[3];
+    $date_array[3] = $date_array[4];
+    $date_array[4] = $temp_val;
+    $new_date_string = implode(',', $date_array);
+    $new_string = "mktime($new_date_string)";
+    $metadata_contents = str_replace($matches[0], $new_string, $metadata_contents);
+  }
 }
