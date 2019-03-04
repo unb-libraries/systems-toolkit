61eb987f71f4134fade88b0d8b42b9e7cc1b3ee4
Brian Cassidy <brian.cassidy@unb.ca>
2019-03-04 13:43:11 -0400

    add updater class w/ command to update remote composer apps

---
+++ lib/systems-toolkit/src/Robo/UpdaterCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace UnbLibraries\SystemsToolkit\Robo;
+
+use Robo\Robo;
+use UnbLibraries\SystemsToolkit\Robo\SystemsToolkitCommand;
+
+/**
+ * Class for Updater Robo commands.
+ */
+class UpdaterCommand extends SystemsToolkitCommand {
+
+  /**
+   * Updates composer-based apps on various servers.
+   *
+   * @throws \Exception
+   *
+   * @command updater:composer-apps
+   */
+  public function composerApps() {
+    $servers = Robo::Config()->get('syskit.updater.composer-apps');
+
+    foreach($servers as $server => $apps) {
+      foreach($apps as $app) {
+        $this->taskSshExec($server)
+          ->exec("cd $app; composer update --prefer-dist")
+          ->run();
+      }
+    }
+  }
+
+}
