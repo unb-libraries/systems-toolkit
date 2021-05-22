63db9853894f0e7c5f83d2b39a4c51d3c77b3d3b
Jacob Sanford <jsanford@unb.ca>
2021-05-22 11:56:03 -0300

    Remove deprecated -a option from prune

--- lib/systems-toolkit/src/Robo/DockerCommandTrait.php
+++ lib/systems-toolkit/src/Robo/DockerCommandTrait.php
@@ -18,9 +18,9 @@ trait DockerCommandTrait {
    */
   public function applicationCleanup() {
     $this->say("Cleaning up dangling containers and volumes:");
-    $this->_exec('docker network prune -af');
-    $this->_exec('docker volume prune -af');
-    $this->_exec('docker container prune -af');
+    $this->_exec('docker network prune -f');
+    $this->_exec('docker volume prune -f');
+    $this->_exec('docker container prune -f');
   }
 
 }
