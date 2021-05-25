1ac326fc4d504651556e4d73adb7c4e749e58d62
Jacob Sanford <jsanford@unb.ca>
2021-05-25 08:56:22 -0300

    Add ProgressLoggerTrait

---
+++ lib/systems-toolkit/src/Robo/ProgressLoggerTrait.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace UnbLibraries\SystemsToolkit\Robo;
+
+use Psr\Log\LogLevel;
+use TrashPanda\ProgressBarLog\ProgressBarLog;
+
+/**
+ * Class for ProgressLoggerTrait Robo commands.
+ */
+trait ProgressLoggerTrait {
+
+  /**
+   * The progress logger.
+   *
+   * @var string
+   */
+  protected $progressLogger;
+
+}
