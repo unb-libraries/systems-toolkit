adf48d80ec40729189c493f82d960558de015eed
Jacob Sanford <jsanford@unb.ca>
2022-02-18 08:25:29 -0400

    Escape special characters in version updates

--- lib/systems-toolkit/data/multiple-modify-scripts/updateReadmeFilesPHP8.sh
+++ lib/systems-toolkit/data/multiple-modify-scripts/updateReadmeFilesPHP8.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-# vendor/bin/syskit github:multiple-repo:script-modify '' 'dockworker' 'Update README to reflect new workflow paths' ~/gitDev/systems-toolkit/lib/systems-toolkit/data/multiple-modify-scripts/updateReadmeFilesPHP8.sh --yes --multi-repo-delay=120 --skip-commit-prefix
-sed -i -E 's|(dockworker.*": ")^4|\1^5|g' ./composer.json
-sed -i -E 's|(dockworker.*": ")~4|\1^5|g' ./composer.json
+# vendor/bin/syskit github:multiple-repo:script-modify '' 'dockworker' 'Update README, CI deployments to reflect new workflow paths' ~/gitDev/systems-toolkit/lib/systems-toolkit/data/multiple-modify-scripts/updateReadmeFilesPHP8.sh --yes --multi-repo-delay=120 --skip-commit-prefix
+sed -i -E 's|(dockworker.*": ")\^4|\1^5|g' ./composer.json
+sed -i -E 's|(dockworker.*": ")\~4|\1^5|g' ./composer.json
 composer install
 vendor/bin/dockworker readme:update
+rm -f .github/workflows/test-suite.yaml
+vendor/bin/dockworker ci:update-workflow-file
 git restore composer.json
