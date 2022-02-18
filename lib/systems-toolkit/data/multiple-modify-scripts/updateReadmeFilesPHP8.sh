2ad779732ac32072ccf9d67d170fcd0c9bbf5379
Jacob Sanford <jsanford@unb.ca>
2022-02-18 07:17:43 -0400

    Add multi-repo script updateReadmeFilesPHP8.sh

---
+++ lib/systems-toolkit/data/multiple-modify-scripts/updateReadmeFilesPHP8.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# vendor/bin/syskit github:multiple-repo:script-modify '' 'dockworker' 'Update README to reflect new workflow paths' ~/gitDev/systems-toolkit/lib/systems-toolkit/data/multiple-modify-scripts/updateReadmeFilesPHP8.sh --yes --multi-repo-delay=120 --skip-commit-prefix
+sed -i -E 's|(dockworker.*": ")[^~]4|\1^5|g' ./composer.json
+composer install
+vendor/bin/dockworker readme:update
+git restore composer.json
