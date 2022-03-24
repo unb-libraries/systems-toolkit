unb-libraries/systems-toolkit

commit 7d8ca7521395d5375ba110b75b9fb8ebfdba3558

Allow configurable temporary directory

lib/systems-toolkit/src/Robo/NewspapersLibUnbCaIngestCommand.php
@@ -92,7 +92,7 @@ class NewspapersLibUnbCaIngestCommand extends OcrCommand {
     $id,
     array $options = [
       'instance-uri' => 'http://localhost:3095',
-      'output-dir' => '/tmp',
+      'output-dir' => $this->tmpDir,
     ]
   ) {
     $local_file = $this->getPageImage($id, $options);
@@ -140,7 +140,7 @@ class NewspapersLibUnbCaIngestCommand extends OcrCommand {
     $id,
     array $options = [
       'instance-uri' => 'http://localhost:3095',
-      'output-dir' => '/tmp',
+      'output-dir' => $this->tmpDir,
     ]
   ) {
     $this->drupalRestUri = $options['instance-uri'];
@@ -161,7 +161,10 @@ class NewspapersLibUnbCaIngestCommand extends OcrCommand {
    *
    * @throws \GuzzleHttp\Exception\GuzzleException
    */
-  private function downloadPageEntityImageFile($page_details, $output_dir = '/tmp') {
+  private function downloadPageEntityImageFile($page_details, $output_dir = '') {
+    if (empty($output_dir)) {
+      $output_dir = $this->tmpDir;
+    }
     if (!empty($page_details->page_image[0]->url)) {
       $page_uri = $page_details->page_image[0]->url;
       $path_parts = pathinfo($page_uri);

lib/systems-toolkit/src/Git/GitRepo.php
@@ -3,7 +3,6 @@
 namespace UnbLibraries\SystemsToolkit\Git;
 
 use CzProject\GitPhp\Git;
-use CzProject\GitPhp\GitRepository;
 
 /**
  * Base class for GitRepo object. Wrapper for Cz\Git\GitRepository.
@@ -40,9 +39,15 @@ class GitRepo {
 
   /**
    * Create and set the temporary directory location.
+   *
+   * @param string $tmp_root
+   *   Optional, the root path for the temp dir. Optional, defaults to system.
    */
-  protected function setTempDir() {
-    $tempfile = tempnam(sys_get_temp_dir(), 'syskit_');
+  protected function setTempDir(string $tmp_root = '') {
+    if (empty($tmp_root)) {
+      $tmp_root = sys_get_temp_dir();
+    }
+    $tempfile = tempnam($tmp_root, 'syskit_');
     if (file_exists($tempfile)) {
       unlink($tempfile);
     }
@@ -58,7 +63,7 @@ class GitRepo {
   }
 
   /**
-   * Get a specific commit from the repository.
+   * Gets a specific commit from the repository.
    *
    * @param string $num
    *   The index of the commit, typically a number.
@@ -66,20 +71,22 @@ class GitRepo {
    * @return array
    *   The commit information array.
    */
-  public function getCommit($num) {
+  public function getCommit(string $num) {
     return $this->commits[$num];
   }
 
   /**
-   * Set up the object from a clone to a local, temporary directory.
+   * Sets up the object from a clone to a local, temporary directory.
    *
    * @param string $repo_url
-   *   The github clone URL of the repository.
+   *   The GitHub clone URL of the repository.
+   * @param string $tmp_root
+   *   Optional, the root path for the temp dir. Optional, defaults to system.
    *
    * @throws \Exception
    */
-  private function setCloneToTempDir($repo_url) {
-    $this->setTempDir();
+  private function setCloneToTempDir(string $repo_url, string $tmp_root = '') {
+    $this->setTempDir($tmp_root);
     $git = new Git();
     $this->repo = $git->cloneRepository(
       $repo_url,
@@ -90,19 +97,21 @@ class GitRepo {
   }
 
   /**
-   * Factory to create this object from a github clone URL.
+   * Creates this object from a GitHub clone URL.
    *
    * @param string $repo_url
-   *   The github clone URL of the repository.
+   *   The GitHub clone URL of the repository.
+   * @param string $tmp_root
+   *   Optional, the root path for the temp dir. Optional, defaults to system.
    *
    * @throws \Exception
    *
    * @return \UnbLibraries\SystemsToolkit\Git\GitRepo
    *   The GitRepo object.
    */
-  public static function setCreateFromClone($repo_url) {
+  public static function setCreateFromClone(string $repo_url, string $tmp_root = '') {
     $repo = new static();
-    $repo->setCloneToTempDir($repo_url);
+    $repo->setCloneToTempDir($repo_url, $tmp_root);
     return $repo;
   }
 
@@ -135,7 +144,7 @@ class GitRepo {
    * @return mixed
    *   The commit message, FALSE if the commit does not exist in the repository.
    */
-  public function getCommitMessage($hash) {
+  public function getCommitMessage(string $hash) {
     foreach ($this->commits as $commit) {
       if ($commit['hash'] == $hash) {
         return $commit['message'];

lib/systems-toolkit/src/Robo/BulkDockworkerCommand.php
@@ -160,7 +160,7 @@ class BulkDockworkerCommand extends SystemsToolkitCommand {
     );
 
     foreach ($this->options['namespaces'] as $namespace) {
-      $repo = GitRepo::setCreateFromClone($repository['ssh_url']);
+      $repo = GitRepo::setCreateFromClone($repository['ssh_url'], $this->tmpDir);
       $repo->repo->checkout($namespace);
       $repo_path = $repo->repo->getRepositoryPath();
       $this->io()->note('Installing Dockworker...');

lib/systems-toolkit/src/Robo/DrupalUpdatesCommand.php
@@ -421,7 +421,7 @@ class DrupalUpdatesCommand extends SystemsToolkitCommand {
 
       $repo_uri = "git@github.com:{$update_data['vcsOwner']}/{$update_data['vcsRepository']}.git";
       $this->say("Cloning $repo_uri...");
-      $repo = GitRepo::setCreateFromClone($repo_uri);
+      $repo = GitRepo::setCreateFromClone($repo_uri, $this->tmpDir);
       $repo->repo->checkout($branch);
       $repo_path = $repo->getTmpDir();
       $repo_build_file_path = "$repo_path/build/composer.json";
@@ -453,7 +453,7 @@ class DrupalUpdatesCommand extends SystemsToolkitCommand {
               $new_content = json_encode($composer_file, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . "\n";
               file_put_contents($repo_build_file_path, $new_content);
 
-              $sort_command = "jq --indent 2 --sort-keys . $repo_build_file_path > /tmp/composer-syskit-sort.json && mv /tmp/composer-syskit-sort.json $repo_build_file_path";
+              $sort_command = "jq --indent 2 --sort-keys . $repo_build_file_path > $this->tmpDir/composer-syskit-sort.json && mv $this->tmpDir/composer-syskit-sort.json $repo_build_file_path";
               shell_exec($sort_command);
 
               $this->say($commit_message);

lib/systems-toolkit/src/Robo/DziTilerCommand.php
@@ -101,7 +101,7 @@ class DziTilerCommand extends SystemsToolkitCommand {
     }
 
     // Remove temporary files from previous runs.
-    shell_exec('sudo rm -rf /tmp/dzi/*');
+    shell_exec("sudo rm -rf $this->tmpDir/dzi/*");
 
     foreach ($this->recursiveFiles as $file_to_process) {
       $dzi_file_path_info = pathinfo($file_to_process);
@@ -195,7 +195,7 @@ class DziTilerCommand extends SystemsToolkitCommand {
     ]
   ) {
     $dzi_file_path_info = pathinfo($file);
-    $tmp_dir = "/tmp/dzi/{$dzi_file_path_info['filename']}";
+    $tmp_dir = "$this->tmpDir/dzi/{$dzi_file_path_info['filename']}";
 
     return $this->taskExecStack()
       ->stopOnFail()

lib/systems-toolkit/src/Robo/GitHubRepoCherryPickCommand.php
@@ -14,7 +14,7 @@ class GitHubRepoCherryPickCommand extends SystemsToolkitCommand {
   use GitHubMultipleInstanceTrait;
 
   public const ERROR_MISSING_REPOSITORY = 'The repository [%s] was not found in any of your configured organizations.';
-  public const FILE_SOURCE_PATCH = '/tmp/syskit_tmp_cherry_patch.txt';
+  public const FILE_SOURCE_PATCH = 'syskit_tmp_cherry_patch.txt';
   public const MESSAGE_BEGINNING_CHERRY_PICK = 'Starting cherry pick operation from [%s] onto all repositories matching topics [%s] and name [%s]';
   public const MESSAGE_CHERRY_PATCH_FAILED = 'Patch cannot apply to [%s/%s]';
   public const MESSAGE_CHERRY_PATCH_SUCCESS = 'Patch successfully applied to [%s/%s]';
@@ -101,7 +101,7 @@ class GitHubRepoCherryPickCommand extends SystemsToolkitCommand {
     $this->sourceRepo = $this->getRepositoryExists($source_repository);
 
     // Instantiate local source repo.
-    $source_repo = GitRepo::setCreateFromClone($this->sourceRepo['ssh_url']);
+    $source_repo = GitRepo::setCreateFromClone($this->sourceRepo['ssh_url'], $this->tmpDir);
 
     // Ask which Commit to Rebase.
     $this->say(sprintf(self::MESSAGE_TITLE_REPO_COMMIT_LIST, $source_repository));
@@ -118,7 +118,7 @@ class GitHubRepoCherryPickCommand extends SystemsToolkitCommand {
       [
         'diff',
         '--unified=0',
-        '--output=' . self::FILE_SOURCE_PATCH,
+        '--output=' . $this->tmpDir . '/' . self::FILE_SOURCE_PATCH,
         "$cherry_hash~1",
         $cherry_hash,
       ]
@@ -173,11 +173,11 @@ class GitHubRepoCherryPickCommand extends SystemsToolkitCommand {
             $target_branch
           )
         );
-        $target_repo = GitRepo::setCreateFromClone($repository_data['ssh_url']);
+        $target_repo = GitRepo::setCreateFromClone($repository_data['ssh_url'], $this->tmpDir);
         $target_repo->repo->checkout($target_branch);
 
         // Apply patch.
-        exec("cd {$target_repo->getTmpDir()} && patch -p1 < " . self::FILE_SOURCE_PATCH . ' && git add .', $output, $return);
+        exec("cd {$target_repo->getTmpDir()} && patch -p1 < " . $this->tmpDir . '/' . self::FILE_SOURCE_PATCH . ' && git add .', $output, $return);
         if ($return) {
           $this->say(sprintf(self::MESSAGE_CHERRY_PATCH_FAILED, $target_branch, $repository_data['name']));
           $this->failedRepos[$repository_data['name']] = "Patch could not be applied.";

lib/systems-toolkit/src/Robo/GitHubRepoRebaseDevToProdCommand.php
@@ -109,7 +109,7 @@ class GitHubRepoRebaseDevToProdCommand extends SystemsToolkitCommand {
             self::MESSAGE_CHECKING_OUT_REPO
           )
         );
-        $repo = GitRepo::setCreateFromClone($repository_data['ssh_url']);
+        $repo = GitRepo::setCreateFromClone($repository_data['ssh_url'], $this->tmpDir);
         if (!self::repoBranchesAreSynchronized($repo, self::UPMERGE_SOURCE_BRANCH, self::UPMERGE_TARGET_BRANCH)) {
           // Rebase.
           $repo->repo->checkout('prod');

lib/systems-toolkit/src/Robo/KubernetesMetadataRepoCommand.php
@@ -254,7 +254,7 @@ class KubernetesMetadataRepoCommand extends SystemsToolkitCommand {
     if (!empty($this->githubRepositories)) {
       // Instantiate local source repo.
       $this->say("Cloning central repo/{$this->options['central-repo-branch']}...");
-      $this->centralMetadataRepo = GitRepo::setCreateFromClone(self::CENTRAL_METADATA_REPO);
+      $this->centralMetadataRepo = GitRepo::setCreateFromClone(self::CENTRAL_METADATA_REPO, $this->tmpDir);
       $this->centralMetadataRepo->repo->checkout($this->options['central-repo-branch']);
     }
 
@@ -328,7 +328,7 @@ class KubernetesMetadataRepoCommand extends SystemsToolkitCommand {
     $this->curLeanRepoNeedsPush = FALSE;
     $this->io->title($this->curLeanRepo['name']);
     $this->say('Cloning lean repo...');
-    $this->curLeanRepoClone = GitRepo::setCreateFromClone($this->curLeanRepo['ssh_url']);
+    $this->curLeanRepoClone = GitRepo::setCreateFromClone($this->curLeanRepo['ssh_url'], $this->tmpDir);
     $this->curLeanRepoClone->repo->checkout(self::LEAN_REPO_BRANCH);
     $this->setCurDockerImage();
     $this->setCurLeanRepoSlug();

commit c2ba7947686275e39a2f3e71d4df358725cccf22

Correct syntax errors introduced in 7d8ca752

lib/systems-toolkit/src/Robo/NewspapersLibUnbCaIngestCommand.php
@@ -90,9 +90,12 @@ class NewspapersLibUnbCaIngestCommand extends OcrCommand {
     string $id,
     array $options = [
       'instance-uri' => 'http://localhost:3095',
-      'output-dir' => $this->tmpDir,
+      'output-dir' => '',
     ]
   ) {
+    if (empty($options['output-dir'])) {
+      $options['output-dir'] = $this->tmpDir;
+    }
     $local_file = $this->getPageImage($id, $options);
     $this->ocrTesseractFile(
       $local_file,
@@ -138,9 +141,12 @@ class NewspapersLibUnbCaIngestCommand extends OcrCommand {
     string $id,
     array $options = [
       'instance-uri' => 'http://localhost:3095',
-      'output-dir' => $this->tmpDir,
+      'output-dir' => '',
     ]
   ) : string {
+    if (empty($options['output-dir'])) {
+      $options['output-dir'] = $this->tmpDir;
+    }
     $this->drupalRestUri = $options['instance-uri'];
     $page_details = $this->getDrupalRestEntity("/digital_serial/digital_serial_page/$id");
     return $this->downloadPageEntityImageFile($page_details, $options['output-dir']);