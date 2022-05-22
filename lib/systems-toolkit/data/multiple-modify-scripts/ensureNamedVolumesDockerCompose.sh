5cdee8cffa7b960c05396cfd9a76bd79045d1be8
Jacob Sanford <jsanford@unb.ca>
2022-05-21 21:44:19 -0300

    Step back toward baseline drupal-only script

--- lib/systems-toolkit/data/multiple-modify-scripts/ensureNamedVolumesDockerCompose.sh
+++ lib/systems-toolkit/data/multiple-modify-scripts/ensureNamedVolumesDockerCompose.sh
@@ -4,70 +4,32 @@ FILE='./docker-compose.yml'
 
 # Ensure Drupal volume is named.
 if grep -q '/app/html/sites/default' "$FILE"; then
-  sed -i "s|- /app/html/sites/default|- drupal-data:/app/html/sites/default|g" "$FILE"
-  cat ./docker-compose.yml | yq '.volumes.drupal-data = {}' | sponge ./docker-compose.yml
+  if grep -q 'drupal-data:' "$FILE"; then
+    echo "Skipping previously-patched drupal-data..."
+  else
+    sed -i "s|- /app/html/sites/default|- drupal-data:/app/html/sites/default|g" "$FILE"
+    cat ./docker-compose.yml | yq '.volumes.drupal-data = {}' | sponge ./docker-compose.yml
+  fi
 fi
 
 # Ensure Drupal mysql volume is named.
 if grep -q 'drupal-mysql-lib-unb-ca' "$FILE"; then
-  cat ./docker-compose.yml | yq '.services.drupal-mysql-lib-unb-ca.volumes |= . + ["mysql-data:/var/lib/mysql"]' | sponge ./docker-compose.yml
-  cat ./docker-compose.yml | yq '.volumes.mysql-data = {}' | sponge ./docker-compose.yml
+  if grep -q 'mysql-data:' "$FILE"; then
+    echo "Skipping previously-patched mysql-data..."
+  else
+    cat ./docker-compose.yml | yq '.services.drupal-mysql-lib-unb-ca.volumes |= . + ["mysql-data:/var/lib/mysql"]' | sponge ./docker-compose.yml
+    cat ./docker-compose.yml | yq '.volumes.mysql-data = {}' | sponge ./docker-compose.yml
+  fi
 fi
 
 # Ensure Drupal solr volume is named.
 if grep -q 'drupal-solr-lib-unb-ca' "$FILE"; then
-  cat ./docker-compose.yml | yq '.services.drupal-solr-lib-unb-ca.volumes |= . + ["solr-data:/opt/solr/server/solr/mycores"]' | sponge ./docker-compose.yml
-  cat ./docker-compose.yml | yq '.volumes.solr-data = {}' | sponge ./docker-compose.yml
-fi
-
-if grep -q 'drupal.mysql.lib.unb.ca' "$FILE"; then
-  cat ./docker-compose.yml | yq '.services."drupal.mysql.lib.unb.ca".volumes |= . + ["mysql-data:/var/lib/mysql"]' | sponge ./docker-compose.yml
-  cat ./docker-compose.yml | yq '.volumes.mysql-data = {}' | sponge ./docker-compose.yml
-fi
-
-if grep -q 'drupal.solr.lib.unb.ca' "$FILE"; then
-  cat ./docker-compose.yml | yq '.services."drupal.solr.lib.unb.ca".volumes |= . + ["solr-data:/opt/solr/server/solr/mycores"]' | sponge ./docker-compose.yml
-  cat ./docker-compose.yml | yq '.volumes.solr-data = {}' | sponge ./docker-compose.yml
-fi
-
-if grep -q 'dspace.postgres.lib.unb.ca' "$FILE"; then
-  cat ./docker-compose.yml | yq '.services."dspace.postgres.lib.unb.ca".volumes |= . + ["postgres-data:/var/lib/postgresql/data/pgdata"]' | sponge ./docker-compose.yml
-  cat ./docker-compose.yml | yq '.volumes.postgres-data = {}' | sponge ./docker-compose.yml
-fi
-
-if grep -q 'mediawiki.mysql.lib.unb.ca' "$FILE"; then
-  cat ./docker-compose.yml | yq '.services."mediawiki.mysql.lib.unb.ca".volumes |= . + ["mysql-data:/var/lib/mysql"]' | sponge ./docker-compose.yml
-  cat ./docker-compose.yml | yq '.volumes.mysql-data = {}' | sponge ./docker-compose.yml
-fi
-
-if grep -q 'mediawiki-mysql-lib-unb-ca' "$FILE"; then
-  cat ./docker-compose.yml | yq '.services.mediawiki-mysql-lib-unb-ca.volumes |= . + ["mysql-data:/var/lib/mysql"]' | sponge ./docker-compose.yml
-  cat ./docker-compose.yml | yq '.volumes.mysql-data = {}' | sponge ./docker-compose.yml
-fi
-
-if grep -q 'omeka.mysql.lib.unb.ca' "$FILE"; then
-  cat ./docker-compose.yml | yq '.services."omeka.mysql.lib.unb.ca".volumes |= . + ["mysql-data:/var/lib/mysql"]' | sponge ./docker-compose.yml
-  cat ./docker-compose.yml | yq '.volumes.mysql-data = {}' | sponge ./docker-compose.yml
-fi
-
-if grep -q 'ospos.mysql.lib.unb.ca' "$FILE"; then
-  cat ./docker-compose.yml | yq '.services."ospos.mysql.lib.unb.ca".volumes |= . + ["mysql-data:/var/lib/mysql"]' | sponge ./docker-compose.yml
-  cat ./docker-compose.yml | yq '.volumes.mysql-data = {}' | sponge ./docker-compose.yml
-fi
-
-if grep -q 'ospos-mysql-lib-unb-ca' "$FILE"; then
-  cat ./docker-compose.yml | yq '.services."ospos-mysql-lib-unb-ca".volumes |= . + ["mysql-data:/var/lib/mysql"]' | sponge ./docker-compose.yml
-  cat ./docker-compose.yml | yq '.volumes.mysql-data = {}' | sponge ./docker-compose.yml
-fi
-
-if grep -q 'unbscholar.postgres.lib.unb.ca' "$FILE"; then
-  cat ./docker-compose.yml | yq '.services."unbscholar.postgres.lib.unb.ca".volumes |= . + ["postgres-data:/var/lib/postgresql/data/pgdata"]' | sponge ./docker-compose.yml
-  cat ./docker-compose.yml | yq '.volumes.postgres-data = {}' | sponge ./docker-compose.yml
-fi
-
-if grep -q 'unbscholar.solr.lib.unb.ca' "$FILE"; then
-  cat ./docker-compose.yml | yq '.services."unbscholar.solr.lib.unb.ca".volumes |= . + ["solr-data:/opt/solr/server/solr/mycores"]' | sponge ./docker-compose.yml
-  cat ./docker-compose.yml | yq '.volumes.solr-data = {}' | sponge ./docker-compose.yml
+  if grep -q 'solr-data:' "$FILE"; then
+    echo "Skipping previously-patched solr-data..."
+  else
+    cat ./docker-compose.yml | yq '.services.drupal-solr-lib-unb-ca.volumes |= . + ["solr-data:/opt/solr/server/solr/mycores"]' | sponge ./docker-compose.yml
+    cat ./docker-compose.yml | yq '.volumes.solr-data = {}' | sponge ./docker-compose.yml
+  fi
 fi
 
 # Remove empty key-value sets.
