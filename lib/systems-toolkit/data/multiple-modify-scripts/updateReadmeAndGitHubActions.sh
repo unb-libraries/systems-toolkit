7484bbf017fda5d49c178223e426395a8824f53d
Jacob Sanford <jsanford@unb.ca>
2022-06-27 07:46:06 -0300

    Update README script to use current commands

--- lib/systems-toolkit/data/multiple-modify-scripts/updateReadmeAndGitHubActions.sh
+++ lib/systems-toolkit/data/multiple-modify-scripts/updateReadmeAndGitHubActions.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# vendor/bin/syskit github:multiple-repo:script-modify '' 'dockworker' 'Update README, gh-workflow' ~/gitDev/systems-toolkit/lib/systems-toolkit/data/multiple-modify-scripts/updateReadmeAndGitHubActions.sh --yes --skip-commit-prefix
 composer install
-vendor/bin/dockworker readme:update
-vendor/bin/dockworker ci:update-workflow-file
+vendor/bin/dockworker readme:file:write
+vendor/bin/dockworker ci:workflow:file:write
