0aac4bcf4bf3c551aaac97be078bf21785ac5b0f
Jacob Sanford <jsanford@unb.ca>
2022-09-12 11:31:27 -0300

    Add addCToolsRestrictionToDrupalComposer.sh

--- lib/systems-toolkit/data/multiple-modify-scripts/addCToolsRestrictionToDrupalComposer.sh
+++ lib/systems-toolkit/data/multiple-modify-scripts/addCToolsRestrictionToDrupalComposer.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-# vendor/bin/syskit github:multiple-repo:script-modify '' 'drupal9' 'Add new security/config to drupal composer.json' ~/gitDev/systems-toolkit/lib/systems-toolkit/data/multiple-modify-scripts/addNewSecurityRequirementsToDrupalComposer.sh --yes --multi-repo-delay=120 --skip-commit-prefix
+# vendor/bin/syskit github:multiple-repo:script-modify '' 'drupal9' 'Automated ctools incompatibility fixes' ~/gitDev/systems-toolkit/lib/systems-toolkit/data/multiple-modify-scripts/addCToolsRestrictionToDrupalComposer.sh --yes --multi-repo-delay=120 --skip-commit-prefix
+# sed -i '/ctools/d' ./config-yml/core.extension.yml config-yml/*.yml
 if [ -f "./build/composer.json" ]; then
-  cd ./build
-  composer config allow-plugins.composer/installers true
-  composer config allow-plugins.cweagans/composer-patches true
-  composer config allow-plugins.drupal/core-composer-scaffold true
-  composer config sort-packages true
-  composer config discard-changes true
-  cd ..
-  jq --sort-keys . ./build/composer.json > /tmp/composer.json && mv /tmp/composer.json ./build/composer.json
-fi
-
-if [ -f "./composer.json" ]; then
-  composer config minimum-stability dev
-  composer config prefer-stable true
-  jq --sort-keys . ./composer.json > /tmp/composer.json && mv /tmp/composer.json ./composer.json
+  if grep -q pathauto "./build/composer.json"; then
+    if grep -q ctools "./config-yml/core.extension.yml"; then
+      read -p "Verify if ctools needs purging..."
+    fi
+  fi
 fi
