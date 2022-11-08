c97a35ecd77250f1d73503ccbd4a269db0fb8a08
Jacob Sanford <jsanford@unb.ca>
2022-11-08 17:12:51 -0400

    Use site slug when updating test deployments

--- lib/systems-toolkit/data/multiple-modify-scripts/updateTestingDeployments.sh
+++ lib/systems-toolkit/data/multiple-modify-scripts/updateTestingDeployments.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # vendor/bin/syskit github:multiple-repo:script-modify '' 'dockworker' 'Update testing deployment to standardize and provide mongo compatibility' ~/gitDev/systems-toolkit/lib/systems-toolkit/data/multiple-modify-scripts/updateTestingDeployments.sh --yes --skip-commit-prefix --multi-repo-delay=120
 FILE='./.dockworker/deployment/k8s/prod/testing.yaml'
+SITE_SLUG="${1//./-}"
+
 if [ -f "$FILE" ]; then
   cat $FILE | yq --prettyPrint '.metadata.namespace = "testing"' | sponge $FILE
   cat $FILE | yq --prettyPrint '.spec.jobTemplate.spec.backoffLimit = 0' | sponge $FILE
@@ -8,6 +10,7 @@ if [ -f "$FILE" ]; then
   cat $FILE | yq --prettyPrint '.spec.jobTemplate.spec.template.spec.nodeSelector.environment = "testing"' | sponge $FILE
   cat $FILE | yq --prettyPrint 'del(.spec.jobTemplate.spec.template.spec.containers[].env[] | select(.name == "DEPLOY_ENV"))' | sponge $FILE
   cat $FILE | yq --prettyPrint '.spec.jobTemplate.spec.template.spec.containers[].env += {"name": "DEPLOY_ENV", "value": "testing"}' | sponge $FILE
+  cat $FILE | yq --prettyPrint 'del(.spec.jobTemplate.spec.template.spec.containers[].env[] | select(.name == "MONGODB_CONNECT_URI"))' | sponge $FILE
   cat $FILE | yq --prettyPrint '.spec.jobTemplate.spec.template.spec.containers[].env += {"name": "MONGODB_CONNECT_URI", "valueFrom": {"secretKeyRef" : {"name": "mongodb", "key": "connect-uri"}}}' | sponge $FILE
-  cp $FILE ~/gitDev/kubernetes-metadata/services/$1/prod/testing-$1.Cronjob.yaml
+  cp $FILE ~/gitDev/kubernetes-metadata/services/$SITE_SLUG/prod/testing-$SITE_SLUG.Cronjob.yaml
 fi
