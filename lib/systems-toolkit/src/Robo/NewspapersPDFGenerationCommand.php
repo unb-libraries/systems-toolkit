1ac243d83c66611aac81902b2b8b2d09b9c5adfc
Jacob Sanford <jsanford@unb.ca>
2024-02-11 07:58:19 -0400

    First implementation of PDF

---
+++ lib/systems-toolkit/src/Robo/NewspapersPDFGenerationCommand.php
@@ -0,0 +1,354 @@
+<?php
+
+namespace UnbLibraries\SystemsToolkit\Robo;
+
+use Robo\Contract\CommandInterface;
+use Robo\Robo;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
+use UnbLibraries\SystemsToolkit\DockerCleanupTrait;
+use UnbLibraries\SystemsToolkit\QueuedParallelExecTrait;
+use UnbLibraries\SystemsToolkit\RecursiveFileTreeTrait;
+use UnbLibraries\SystemsToolkit\Robo\SystemsToolkitCommand;
+
+/**
+ * Class for PdfTilerCommand Robo commands.
+ */
+class NewspapersPDFGenerationCommand extends SystemsToolkitCommand {
+
+  use DockerCleanupTrait;
+  use QueuedParallelExecTrait;
+  use RecursiveFileTreeTrait;
+
+  /**
+   * The docker image to use for Imagemagick commands.
+   *
+   * @var string
+   */
+  private string $imagemagickImage;
+
+  /**
+   * Gets the Tesseract docker image from config.
+   *
+   * @throws \Exception
+   *
+   * @hook init
+   */
+  public function setImagingImage() : void {
+    $this->imagemagickImage = Robo::Config()->get('syskit.imaging.imagemagickImage');
+    if (empty($this->imagemagickImage)) {
+      throw new \Exception(sprintf('The imagemagick docker image has not been set in the configuration file. (imagemagickImage)'));
+    }
+  }
+
+  /**
+   * Generates PDFs for an entire tree.
+   *
+   * @param string $root
+   *   The tree root to parse.
+   * @param string[] $options
+   *   The array of available CLI options.
+   *
+   * @option $extension
+   *   The extensions to match when finding files.
+   * @option $no-pull
+   *   Do not pull docker images prior to running.
+   * @option $prefix
+   *   The prefix to match when finding files.
+   * @option $skip-confirm
+   *   Should the confirmation process be skipped?
+   * @option $skip-existing
+   *   Should images with existing tiles be skipped?
+   * @option $target-gid
+   *   The gid to assign the target files.
+   * @option $target-uid
+   *   The uid to assign the target files.
+   * @option $threads
+   *   The number of threads the process should use.
+   * @option $no-cleanup
+   *   Do not clean up unused docker assets after running needed containers.
+   *
+   * @throws \Exception
+   *
+   * @command pdf:generate:tree
+   */
+  public function pdfFilesTree(
+    string $root,
+    array $options = [
+      'extension' => '.tif',
+      'no-pull' => FALSE,
+      'prefix' => NULL,
+      'skip-confirm' => FALSE,
+      'skip-existing' => FALSE,
+      'target-gid' => '102',
+      'target-uid' => '100',
+      'threads' => NULL,
+      'no-cleanup' => FALSE,
+    ]
+  ) : void {
+    $regex_root = preg_quote($root, '/');
+
+    if (!$options['no-pull']) {
+      $this->setPullImagemagickImage();
+    }
+    $options['no-pull'] = TRUE;
+
+    if (!empty($options['prefix'])) {
+      $glob_path = "$root/{$options['prefix']}*.{$options['extension']}";
+      $this->recursiveFiles = glob($glob_path);
+    }
+    else {
+      $regex = "/^{$regex_root}\/[^\/]+\.{$options['extension']}$/i";
+      $this->recursiveFileTreeRoot = $root;
+      $this->recursiveFileRegex = $regex;
+      $this->setFilesToIterate();
+      $this->getConfirmFiles('Generate PDF files', $options['skip-confirm']);
+    }
+
+    foreach ($this->recursiveFiles as $file_to_process) {
+      $pdf_file_path_info = pathinfo($file_to_process);
+      if ($options['skip-existing'] &&
+        file_exists("{$pdf_file_path_info['dirname']}/{$pdf_file_path_info['filename']}.pdf")
+      ) {
+        $this->say("Skipping file with existing PDFs [$file_to_process]");
+      }
+      else {
+        $this->setAddCommandToQueue($this->getPdfCreateCommand($file_to_process, $options));
+      }
+    }
+    if (!empty($options['threads'])) {
+      $this->setThreads($options['threads']);
+    }
+    $this->setRunProcessQueue('Generate PDF files');
+    if (!$options['no-cleanup']) {
+      $this->applicationCleanup();
+    }
+  }
+
+  /**
+   * Generates PDF Files for a list of files.
+   *
+   * @param string $filepath
+   *   The file containing the file list.
+   * @param string[] $options
+   *   The array of available CLI options.
+   *
+   * @option $no-pull
+   *   Do not pull docker images prior to running.
+   * @option $skip-confirm
+   *   Should the confirmation process be skipped?
+   * @option $skip-existing
+   *   Should images with existing tiles be skipped?
+   * @option $step
+   *   The zoom step to use.
+   * @option $target-gid
+   *   The gid to assign the target files.
+   * @option $target-uid
+   *   The uid to assign the target files.
+   * @option $threads
+   *   The number of threads the process should use.
+   * @option $tile-size
+   *   The tile size to use.
+   * @option $no-cleanup
+   *   Do not clean up unused docker assets after running needed containers.
+   *
+   * @throws \Exception
+   *
+   * @command pdf:generate:from-list
+   */
+  public function pdfFiles(
+    string $filepath,
+    array $options = [
+      'no-pull' => FALSE,
+      'skip-confirm' => FALSE,
+      'skip-existing' => FALSE,
+      'step' => '200',
+      'target-gid' => '102',
+      'target-uid' => '100',
+      'threads' => NULL,
+      'tile-size' => '256',
+      'no-cleanup' => FALSE,
+    ]
+  ) : void {
+    if (!$options['no-pull']) {
+      $this->setPullImagemagickImage();
+    }
+    $options['no-pull'] = TRUE;
+
+    $files_to_process = explode(
+      "\n",
+      file_get_contents($filepath)
+    );
+
+    foreach ($files_to_process as $file_to_process) {
+      $file_to_process = trim($file_to_process);
+      if (!empty($file_to_process)) {
+        $pdf_file_path_info = pathinfo($file_to_process);
+        if ($options['skip-existing'] &&
+          file_exists("{$pdf_file_path_info['dirname']}/{$pdf_file_path_info['filename']}.pdf")
+        ) {
+          $this->say("Skipping file with existing tiles [$file_to_process]");
+        }
+        else {
+          $this->setAddCommandToQueue($this->getPdfCreateCommand($file_to_process, $options));
+        }
+      }
+    }
+
+    if (!empty($options['threads'])) {
+      $this->setThreads($options['threads']);
+    }
+    $this->setRunProcessQueue('Generate PDF files');
+    if (!$options['no-cleanup']) {
+      $this->applicationCleanup();
+    }
+  }
+
+  /**
+   * Generates PDF Files for a specific NBNP issue.
+   *
+   * @param string $root
+   *   The NBNP webtree root file location.
+   * @param string $issue_id
+   *   The issue entity ID to process.
+   * @param string[] $options
+   *   The array of available CLI options.
+   *
+   * @option $no-cleanup
+   *   Do not clean up unused docker assets after running.
+   * @option $no-pull
+   *   Do not pull docker images prior to running.
+   * @option $skip-existing
+   *   Skip any issues with tiles that have been previously generated.
+   * @option $threads
+   *   The number of threads the process should use.
+   *
+   * @command newspapers.lib.unb.ca:issue:generate-pdf
+   *
+   * @throws \Exception
+   */
+  public function nbnpPdfIssue(
+    string $root,
+    string $issue_id,
+    array $options = [
+      'no-cleanup' => FALSE,
+      'no-pull' => FALSE,
+      'skip-existing' => FALSE,
+      'threads' => 1,
+    ]
+  ) : void {
+    $cmd_options = [
+      'extension' => 'jpg',
+      'no-pull' => $options['no-pull'],
+      'prefix' => "{$issue_id}-",
+      'skip-confirm' => TRUE,
+      'skip-existing' => $options['skip-existing'],
+      'target-gid' => '102',
+      'target-uid' => '100',
+      'threads' => $options['threads'],
+      'no-cleanup' => TRUE,
+    ];
+    $this->pdfFilesTree(
+      $root . '/files/serials/pages',
+      $cmd_options
+    );
+    if (!$options['no-cleanup']) {
+      $this->applicationCleanup();
+    }
+  }
+
+  /**
+   * Builds the Robo command used to generate the PDF.
+   *
+   * @param string $file
+   *   The file to parse.
+   * @param string[] $options
+   *   The array of available CLI options.
+   *
+   * @option $tile-size
+   *   The tile size to use.
+   * @option $step
+   *   The zoom step to use.
+   * @option $target-uid
+   *   The uid to assign the target files.
+   * @option $target-gid
+   *   The gid to assign the target files.
+   *
+   * @return \Robo\Contract\CommandInterface
+   *   The Robo command, ready to execute.
+   */
+  private function getPdfCreateCommand(
+    string $file,
+    array $options = [
+      'target-gid' => '102',
+      'target-uid' => '100',
+    ]
+  ) : CommandInterface {
+    $pdf_file_path_info = pathinfo($file);
+    $tmp_dir = "$this->tmpDir/{$pdf_file_path_info['filename']}";
+
+    return $this->taskExecStack()
+      ->stopOnFail()
+      ->exec("sudo rm -rf $tmp_dir")
+      ->exec("mkdir -p $tmp_dir")
+      ->exec("cp $file $tmp_dir")
+      ->exec("docker run -v  $tmp_dir:/data --rm {$this->imagemagickImage} convert -- '$tmp_dir/$file' -quality 100 '$tmp_dir/{$pdf_file_path_info['filename']}.pdf'")
+      ->exec("sudo cp $tmp_dir/{$pdf_file_path_info['filename']}.pdf {$pdf_file_path_info['dirname']}/")
+      ->exec("sudo chown {$options['target-uid']}:{$options['target-gid']} {$pdf_file_path_info['dirname']}/{$pdf_file_path_info['filename']}.pdf")
+      ->exec("sudo rm -rf $tmp_dir");
+  }
+
+  /**
+   * Generates a PDF of an image file.
+   *
+   * @param string $file
+   *   The file to parse.
+   * @param string[] $options
+   *   The array of available CLI options.
+   *
+   * @option $tile-size
+   *   The tile size to use.
+   * @option $step
+   *   The zoom step to use.
+   * @option $target-uid
+   *   The uid to assign the target files.
+   * @option $target-gid
+   *   The gid to assign the target files.
+   * @option $no-pull
+   *   Do not pull docker images prior to running.
+   *
+   * @throws \Symfony\Component\Filesystem\Exception\FileNotFoundException
+   *
+   * @command pdf:generate
+   */
+  public function generatePdfFiles(
+    string $file,
+    array $options = [
+      'no-pull' => FALSE,
+      'skip-existing' => FALSE,
+    ]
+  ) : void {
+    $pdf_file_path_info = pathinfo($file);
+    if (!file_exists($file)) {
+      throw new FileNotFoundException("File $file not Found!");
+    }
+    if (!$options['skip-existing'] ||
+      !file_exists("{$pdf_file_path_info['dirname']}/{$pdf_file_path_info['filename']}.pdf")
+    ) {
+      if (!$options['no-pull']) {
+        $this->setPullImagemagickImage();
+      }
+      $command = $this->getPdfCreateCommand($file, $options);
+      $command->run();
+    }
+  }
+
+  /**
+   * Pulls the docker image required to generate PDF files.
+   *
+   * @command pdf:pull-image
+   */
+  public function setPullImagemagickImage() : void {
+    shell_exec("docker pull {$this->imagemagickImage}");
+  }
+
+}
