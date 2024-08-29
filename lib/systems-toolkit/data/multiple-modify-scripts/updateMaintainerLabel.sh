9bf959a307e3ba72bed30849c68ccdf7ecc3d6f9
Jacob Sanford <jsanford@unb.ca>
2024-08-29 15:27:04 -0300

    Update script usage doc

--- lib/systems-toolkit/data/multiple-modify-scripts/updateMaintainerLabel.sh
+++ lib/systems-toolkit/data/multiple-modify-scripts/updateMaintainerLabel.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# vendor/bin/syskit github:multiple-repo:script-modify '' 'dockworker' 'IN-348 Migrate MAINTAINER in Dockerfile' ~/gitDev/systems-toolkit/lib/systems-toolkit/data/multiple-modify-scripts/updateMaintainerLabel.sh --yes
+# vendor/bin/syskit github:multiple-repo:script-modify '' 'dockworker' 'IN-348 Migrate MAINTAINER in Dockerfile' ~/gitDev/systems-toolkit/lib/systems-toolkit/data/multiple-modify-scripts/updateMaintainerLabel.sh --yes -skip-commit-prefix
+#   org.opencontainers.image.authors="UNB Libraries <libsupport@unb.ca>" \
 
 # If the Dockerfile has a MAINTAINER declaration, pause and wait for input.
 if grep -q "MAINTAINER" Dockerfile; then
