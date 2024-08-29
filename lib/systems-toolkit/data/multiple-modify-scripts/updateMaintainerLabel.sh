65e4aaf4e932def73fbbac9ba4e33aa2086b0736
Jacob Sanford <jsanford@unb.ca>
2024-08-29 07:30:26 -0300

    Add updateMaintainerLabel.sh

---
+++ lib/systems-toolkit/data/multiple-modify-scripts/updateMaintainerLabel.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# vendor/bin/syskit github:multiple-repo:script-modify '' 'dockworker' 'IN-348 Migrate MAINTAINER in Dockerfile' ~/gitDev/systems-toolkit/lib/systems-toolkit/data/multiple-modify-scripts/updateMaintainerLabel.sh --yes
+
+# If the Dockerfile has a MAINTAINER declaration, pause and wait for input.
+if grep -q "MAINTAINER" Dockerfile; then
+  echo "MAINTAINER declaration found in Dockerfile."
+  echo "Please update the MAINTAINER declaration to the LABEL declaration."
+  read -s -n 1 -p "Press any key to continue . . ."
+fi
+
